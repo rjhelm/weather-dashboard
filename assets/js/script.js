0b6d63f22def84d9f91bab15f6e4c602cdc4adf0
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-03-05 14:59:44 -0700

    started testing styles on the page in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ let uvEl = document.createElement("h4");
 let uvDisplay = document.createElement("div");
 
 // 5 day forecast HTML Variables
-let fiveDay = document.querySelector("#five-day");
+let fiveDay = document.querySelector("#five-day-result");
 
 // Search HTML variables
 let searchCard = document.querySelector("#search-wrapper");
@@ -47,31 +47,31 @@ let getWeather = function(city) {
     .then(function (response) {
         tempDiv.classList = 'temp-div';
         responseContainer.appendChild(tempDiv);
-        cityDiv.classList = 'detail-div';
+        cityDiv.classList = 'city-div';
         responseContainer.appendChild(cityDiv);
         // Display the user's search entry for the city
         cityEl.innerHTML =
-          "<h2 class='secondary-text'>Current Weather for <span class='font-weight-bold'>" + response.name + 
+          "<h2 class='created-text'>Current Weather for <span class='font-weight-bold'>" + response.name + 
           "</span><h2><br><img class='icon' src='http://openweathermap.org/img/w/" + response.weather[0].icon +
-          ".png' alt=Current weather icon/><h2 class='font-weight-bold secondary-text'>" + date + "</h2><br>";
+          ".png' alt=Current weather icon/><h2 class='font-weight-bold created-text'>" + date + "</h2><br>";
         tempDiv.appendChild(cityEl);
         // Display the fetched current temperature value
         tempEl.innerHTML =
-          "<h3 class='secondary-text'>Current Temperature:<span class='font-weight-bold'>" +
+          "<h3 class='created-text'>Current Temperature:<span class='font-weight-bold'>" +
           " " +
           Math.round(response.main.temp) +
           "&#176F</span></h3><br>";
         tempDiv.appendChild(cityTempEl);
         // Display the fetched humidity value
         humidityEl.innerHTML =
-          "<h4 class='secondary-text'>Humidity:<span class='font-weight-bold'>" +
+          "<h4 class='created-text'>Humidity:<span class='font-weight-bold'>" +
           " " +
           response.main.humidity +
           "%</span></h4>";
         cityDiv.appendChild(humidityEl);
         // Display the fetched wind speed value
         windEl.innerHTML =
-          "<h4 class='secondary-text'>Wind Speed:<span class='font-weight-bold'>" +
+          "<h4 class='created-text'>Wind Speed:<span class='font-weight-bold'>" +
           " " +
           Math.round(response.wind.speed) +
           " MPH</span></h4>";
@@ -84,7 +84,7 @@ let getWeather = function(city) {
     })
     .then(function (uvResponse) {
         uvContainer.setAttribute("id", "uv-value");
-        uvContainer.classList = "secondary-text uv-class";
+        uvContainer.classList = "created-text uv-container";
         cityDiv.appendChild(uvContainer);
         let uvValue = uvResponse.value;
         uvEl.innerHTML = "UV Index: ";
@@ -107,29 +107,29 @@ let getWeather = function(city) {
     })
     .then(function (fiveDayResponse) {
         for (let i = 1; i < 6; i++) {
-            let forecastEl = document.createElement("div");
-            forecastEl.classList = "forecast-card card-body rounded-md border-dark bg-info text-light"
-            fiveDay.appendChild(forecastEl);
+            let fiveDayEl = document.createElement("div");
+            fiveDayEl.classList = "five-card card-body rounded-md border-dark bg-info text-light"
+            fiveDay.appendChild(fiveDayEl);
 
             let dateDiv = document.createElement("div");
-            dateDiv.classList = "secondary-text card-title";
+            dateDiv.classList = "created-text card-title";
             let forecastDate = moment.utc(fiveDayResponse.daily[i].dt * 1000).format("MM/DD/YYYY");
             dateDiv.innerHTML = "<h4 class='font-weight-bold>" + forecastDate + "</h4>";
-            forecastEl.appendChild(dateDiv);
+            fiveDayEl.appendChild(dateDiv);
                 
             let iconDiv = document.createElement("div");
-            iconDiv.innerHTML = "<img src='http://openweathermap.org/img/w/" + fiveDayResponse.daily[i].weather[0].icon + ".png' class='forecast-icon' alt=Current weather icon/>";
-            forecastEl.appendChild(iconDiv);
+            iconDiv.innerHTML = "<img src='http://openweathermap.org/img/w/" + fiveDayResponse.daily[i].weather[0].icon + ".png' class='five-day-icon' alt=Current weather icon/>";
+            fiveDayEl.appendChild(iconDiv);
 
             let tempDiv = document.createElement("div");
-            tempDiv.classList = "card-text secondary-text";
+            tempDiv.classList = "card-text created-text";
             tempDiv.innerHTML = "<h5>Day Temp:<span>" + " " + Math.round(fiveDayResponse.daily[i].temp.day) + "&#176F</span></h5>" + "<h5>Night Temp:<span>" + " " + Math.round(fiveDayResponse.daily[i].temp.night) + " &#176F</span></h5>";
-            forecastEl.appendChild(tempDiv);
+            fiveDayEl.appendChild(tempDiv);
 
             let humidityDiv = document.createElement("div");
-            humidityDiv.classList = "card-text secondary-text";
+            humidityDiv.classList = "card-text created-text";
             humidityDiv.innerHTML = "<h5>Humidity:<span>" + " " + fiveDayResponse.daily[i].humidity + "%</span></h5>";
-            forecastEl.appendChild(humidityDiv);
+            fiveDayEl.appendChild(humidityDiv);
         }
     })
     .catch(function (error){
